bd6a13a32dd4d0229d58718c72404502315901b6
breakerthb <breakerthb@126.com>
2017-01-25 07:45:54 +0000

    Add static class to make utils method

---
+++ Utils/Shape.cpp
@@ -0,0 +1,30 @@
+﻿#include "Shape.h"
+
+bool Shape::CheckPointInRect(Point& point, Rect& rect)
+{
+	if (point.GetX() < rect.GetStartPoint().GetX() || point.GetX() > rect.GetEndPoint().GetX() ||
+		point.GetY() < rect.GetStartPoint().GetY() || point.GetY() > rect.GetEndPoint().GetY())
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+bool Shape::CheckIntersect(Rect& rectA, Rect& rectB)
+{
+	if (CheckPointInRect(rectA.GetStartPoint(), rectB) ||
+		CheckPointInRect(rectA.GetEndPoint(), rectB) ||
+		CheckPointInRect(rectA.GetTRPoint(), rectB) ||
+		CheckPointInRect(rectA.GetBLPoint(), rectB))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+}
---
+++ Utils/Shape.h
@@ -0,0 +1,13 @@
+﻿#ifndef __SHAPE_H__
+#define __SHAPE_H__
+
+#include "../Model/Rect.h"
+
+class Shape
+{
+public:
+	static bool CheckPointInRect(Point& point, Rect& rect);
+	static bool CheckIntersect(Rect& rectA, Rect& rectB);
+};
+
+#endif
